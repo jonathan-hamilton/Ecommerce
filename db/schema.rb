db8211130b2cebe46c632535eaf09884e66b1c51
jonathan-hamilton <jonathan_hamilton@me.com>
2021-03-13 21:18:26 -0800

    added customer scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_051251) do
+ActiveRecord::Schema.define(version: 2021_03_14_051600) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "line_items", force: :cascade do |t|
     t.integer "quantity"
