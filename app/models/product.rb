c443a88f50cdb9d15c18fd80ab753dc349ea8b05
jonathan-hamilton <jonathan_hamilton@me.com>
2021-04-07 19:37:03 -0700

    wip

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,6 @@ class Product < ApplicationRecord
     validates :productName, presence: true
     validates :description, presence: true
     validates :price, presence: true
-    # validates :category_id, presence: true
-    # validates :type_id, presence: true
-    # validates :brand_id, presence: true
 
     belongs_to :category
     belongs_to :type
