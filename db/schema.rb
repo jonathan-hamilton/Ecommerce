93c550a31e3b8dbe593e013a18c7c92223c423d9
jonathan-hamilton <jonathan_hamilton@me.com>
2021-04-28 21:10:22 -0700

    product card images and migration for pic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_023431) do
+ActiveRecord::Schema.define(version: 2021_04_29_014008) do
 
   create_table "brands", force: :cascade do |t|
     t.string "brandName"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2021_04_28_023431) do
     t.integer "brand_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "pic"
   end
 
   create_table "stores", force: :cascade do |t|
