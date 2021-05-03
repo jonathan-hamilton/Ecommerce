1796308ff36424aa975d82094a0c2721d189045b
jonathan-hamilton <jonathan_hamilton@me.com>
2021-05-02 17:55:19 -0700

    product detail styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   config.assets.debug = true
 
   # Suppress logger output for asset requests.
-  config.assets.quiet = true
+  # config.assets.quiet = true
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
@@ -59,4 +59,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
